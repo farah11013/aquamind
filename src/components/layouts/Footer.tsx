35a51e061f0e390a79c7711cbad80795d912c3be
miaoda <miaoda@baidu.com>
2026-01-29 16:44:20 +0800

    提交代码 no sync

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -36,7 +36,7 @@ export function Footer() {
         </div>
         
         <div className="border-t border-border mt-8 pt-6 text-center text-sm text-muted-foreground">
-          <p>© 2026 AquaMind. Created by Cahcet-HackTech Team. All rights reserved.</p>
+          <p>{"© 2026 AquaMind. Created by Cahcet-HackTech Team."}</p>
         </div>
       </div>
     </footer>
